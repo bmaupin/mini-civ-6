2125ad089c5619900a003f5b51b87b2cf6ea41f7
bmaupin <bmaupin@users.noreply.github.com>
2025-06-18 13:15:26 -0400

    fix: Fix game speed hack icon override
    When Index was unquoted there was an error in database.log, but not when it was single quoted even though it was incorrect (it would be interpreted as a value in that case). It needs to be double quoted.

--- src/Data/MiniCivVI_GameSpeedIconsHack.sql
+++ src/Data/MiniCivVI_GameSpeedIconsHack.sql
@@ -1,3 +1,5 @@
-INSERT OR REPLACE INTO IconDefinitions (Name, Atlas, 'Index')
-SELECT 'ICON_GAMESPEED_STANDARD_CUSTOM', Atlas, 'Index'
+-- NOTE: Index is a reserved keyword so it has to be escaped with double quotes
+--       (single quotes won't work)
+INSERT OR REPLACE INTO IconDefinitions (Name, Atlas, "Index")
+SELECT 'ICON_GAMESPEED_STANDARD_CUSTOM', Atlas, "Index"
 FROM IconDefinitions WHERE Name = 'ICON_GAMESPEED_STANDARD';
