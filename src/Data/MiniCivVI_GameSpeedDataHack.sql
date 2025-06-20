ef14f995a6d8b6e956020678e78460a54ca4bda1
bmaupin <bmaupin@users.noreply.github.com>
2025-06-20 14:04:44 -0400

    style: Replace tabs with spaces

--- src/Data/MiniCivVI_GameSpeedDataHack.sql
+++ src/Data/MiniCivVI_GameSpeedDataHack.sql
@@ -3,14 +3,14 @@ SELECT 'GAMESPEED_STANDARD_CUSTOM', Kind
 FROM Types WHERE Type = 'GAMESPEED_STANDARD';
 
 INSERT OR REPLACE INTO GameSpeeds (
-	GameSpeedType, Name, Description,
-	CostMultiplier, CivicUnlockMaxCost,
-	CivicUnlockPerTurnDrop, CivicUnlockMinCost
+    GameSpeedType, Name, Description,
+    CostMultiplier, CivicUnlockMaxCost,
+    CivicUnlockPerTurnDrop, CivicUnlockMinCost
 )
 SELECT
-	'GAMESPEED_STANDARD_CUSTOM', Name, Description,
-	CostMultiplier, CivicUnlockMaxCost,
-	CivicUnlockPerTurnDrop, CivicUnlockMinCost
+    'GAMESPEED_STANDARD_CUSTOM', Name, Description,
+    CostMultiplier, CivicUnlockMaxCost,
+    CivicUnlockPerTurnDrop, CivicUnlockMinCost
 FROM GameSpeeds
 WHERE GameSpeedType = 'GAMESPEED_STANDARD';
 
