240a81c08e65db4b2b53ba1d39b9076124f5dc5a
bmaupin <bmaupin@users.noreply.github.com>
2025-06-29 15:42:59 -0400

    feat: Also remove techs that have no unlocks
    ... as part of cleanup

--- src/Data/MiniCivVI_Cleanup.sql
+++ src/Data/MiniCivVI_Cleanup.sql
@@ -7,8 +7,6 @@ DELETE FROM Civics WHERE
     SELECT CivicType FROM CivicModifiers
   ) AND
   CivicType NOT IN (
-    SELECT PrereqCivic FROM Policies WHERE PrereqCivic IS NOT NULL
-    UNION
     SELECT PrereqCivic FROM Adjacency_YieldChanges WHERE PrereqCivic IS NOT NULL
     UNION
     SELECT PrereqCivic FROM Buildings WHERE PrereqCivic IS NOT NULL
@@ -29,6 +27,8 @@ DELETE FROM Civics WHERE
     UNION
     SELECT PrereqCivic FROM Improvement_ValidTerrains WHERE PrereqCivic IS NOT NULL
     UNION
+    SELECT PrereqCivic FROM Policies WHERE PrereqCivic IS NOT NULL
+    UNION
     SELECT PrereqCivic FROM Projects WHERE PrereqCivic IS NOT NULL
     UNION
     SELECT PrereqCivic FROM Resources WHERE PrereqCivic IS NOT NULL
@@ -38,4 +38,46 @@ DELETE FROM Civics WHERE
     SELECT PrereqCivic FROM UnitCommands WHERE PrereqCivic IS NOT NULL
     UNION
     SELECT PrereqCivic FROM UnitOperations WHERE PrereqCivic IS NOT NULL
+  );
+
+DELETE FROM Technologies WHERE
+  -- This excludes TECH_FUTURE_TECH, which is special
+  Repeatable != 1 AND
+  TechnologyType NOT IN (
+    SELECT TechnologyType FROM TechnologyModifiers
+  ) AND
+  TechnologyType NOT IN (
+    SELECT PrereqTech FROM Adjacency_YieldChanges WHERE PrereqTech IS NOT NULL
+    UNION
+    SELECT PrereqTech FROM Buildings WHERE PrereqTech IS NOT NULL
+    UNION
+    SELECT InitiatorPrereqTech FROM DiplomaticActions WHERE InitiatorPrereqTech IS NOT NULL
+    UNION
+    SELECT PrereqTech FROM DiplomaticVisibilitySources WHERE PrereqTech IS NOT NULL
+    UNION
+    SELECT PrereqTech FROM Districts WHERE PrereqTech IS NOT NULL
+    UNION
+    SELECT PrereqTech FROM Improvements WHERE PrereqTech IS NOT NULL
+    UNION
+    SELECT PrereqTech FROM Improvement_BonusYieldChanges WHERE PrereqTech IS NOT NULL
+    UNION
+    SELECT PrereqTech FROM Improvement_Tourism WHERE PrereqTech IS NOT NULL
+    UNION
+    SELECT PrereqTech FROM Improvement_ValidFeatures WHERE PrereqTech IS NOT NULL
+    UNION
+    SELECT PrereqTech FROM Improvement_ValidTerrains WHERE PrereqTech IS NOT NULL
+    UNION
+    SELECT PrereqTech FROM Policies WHERE PrereqTech IS NOT NULL
+    UNION
+    SELECT PrereqTech FROM Projects WHERE PrereqTech IS NOT NULL
+    UNION
+    SELECT PrereqTech FROM Resources WHERE PrereqTech IS NOT NULL
+    UNION
+    SELECT PrereqTech FROM Resource_Harvests WHERE PrereqTech IS NOT NULL
+    UNION
+    SELECT PrereqTech FROM Units WHERE PrereqTech IS NOT NULL
+    UNION
+    SELECT PrereqTech FROM UnitCommands WHERE PrereqTech IS NOT NULL
+    UNION
+    SELECT PrereqTech FROM UnitOperations WHERE PrereqTech IS NOT NULL
   );
