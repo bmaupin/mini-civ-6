4b66bd2a39c7754727e52f073dc7d7f43876b5bb
bmaupin <bmaupin@users.noreply.github.com>
2025-07-03 13:02:08 -0400

    chore: Install script improvements
    Manual mod installation should be much easier with Civ 6 so the install script can just be used for local development:
    
    - Document manual mod install
    - Validate files to make sure they match the .modinfo file to catch errors preventatively
    - Put timestamp in mod description
    - List mod directory contents after install as an extra safeguard

--- README.md
+++ README.md
@@ -36,6 +36,15 @@ If city states are set to 0 and Barbarian Clans mode isn't checked, city state f
 
 See [Credits](#credits) below for more information
 
+## Manual installation
+
+1. Download the repository source file from [Releases](https://github.com/bmaupin/mini-civ-6/releases) and extract it
+1. Rename the `src` directory to `Mini Civ 6` and copy it to [your Mods directory](https://www.pcgamingwiki.com/wiki/Sid_Meier%27s_Civilization_VI#Configuration_file.28s.29_location):
+
+    - Linux: ~/.local/share/aspyr-media/Sid Meier's Civilization VI/Mods
+    - Mac: /Users/[user]/Library/Application Support/Sid Meier's Civilization VI/Mods
+    - Windows: Documents/My Games/Sid Meier's Civilization VI/Mods
+
 ## Credits
 
 #### Civic/tech boosted popups disabled
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,56 @@
 #!/usr/bin/env bash
 
+# NOTE: This install script is only meant for local development. For all other purposes,
+#       see README.md for installation instructions.
+
 if ! command -v yq &> /dev/null; then
     echo "yq is not installed. Please install yq to run this script."
     exit 1
 fi
 
 mod_name=$(yq -p xml -oy ".Mod.Properties.Name" src/*.modinfo)
+
+# Validate files
+
+pushd src > /dev/null
+
+# ⚠️ This will fail if there are spaces in any of the file names
+action_files=($(yq -p xml -oy '.Mod.FrontEndActions.*.File | .[]? // .' "${mod_name}.modinfo"))
+action_files+=($(yq -p xml -oy ".Mod.InGameActions.ReplaceUIScript[].Properties.LuaReplace" Mini\ Civ\ 6.modinfo))
+action_files+=($(yq -p xml -oy '.Mod.InGameActions.UpdateDatabase[].File | select (.) | .[]? // .' "${mod_name}.modinfo"))
+action_files+=($(yq -p xml -oy '.Mod.InGameActions.UpdateIcons[].File | select (.) | .[]? // .' "${mod_name}.modinfo"))
+
+files_section_files=($(yq -p xml -oy '.Mod.Files.File | .[]? // .' "${mod_name}.modinfo"))
+
+for file in "${action_files[@]}"; do
+    if [[ ! -f "$file" ]]; then
+        echo "Error: Missing file: $file"
+        exit 1
+    fi
+done
+
+for file in "${files_section_files[@]}"; do
+    if [[ ! -f "$file" ]]; then
+        echo "Error: Missing file: $file"
+        exit 1
+    fi
+done
+
+for file in "${action_files[@]}"; do
+  found=false
+  for checkfile in "${files_section_files[@]}"; do
+    if [[ "$file" == "$checkfile" ]]; then
+      found=true
+      break
+    fi
+  done
+  if [[ "$found" == false ]]; then
+    echo "Error: File '$file' from action_files is not listed in the <Files> section"
+    exit 1
+  fi
+done
+
+popd >/dev/null
 
 echo "Installing mod: ${mod_name}"
 
@@ -20,8 +65,14 @@ if [[ -f "/home/$USER/.steam/steam/steamapps/common/Sid Meier's Civilization VI/
     # user_directory="/home/${USER}/.steam/steam/steamapps/compatdata/289070/pfx/drive_c/users/steamuser/Documents/My Games/Sid Meier's Civilization VI"
 fi
 
-echo "Copying mod files ..."
+# Inject the current date into the mod description. This makes it easier to tell if the
+# mod has been updated when doing development.
+sed -i "s|\(<Description>\)[^<]*\(</Description>\)|\1$(date)\2|" "src/${mod_name}.modinfo"
+
+echo
 mod_directory="${user_directory}/Mods/${mod_name}"
 # Always clean up the mod directory first in case we delete files from the mod source
 rm -rf "${mod_directory}"
 cp -ar src "${mod_directory}"
+
+ls -l "${user_directory}/Mods"
