393a438a0eb4090f167adbbde1cf2e2204f68958
bmaupin <bmaupin@users.noreply.github.com>
2025-07-14 12:48:06 -0400

    feat: Switch to the governments tab automatically
    ... if the policy tab is shown. Also remove the gap caused by hiding the policies tab button, disable Lua global warnings, and replace tabs with spaces.

--- src/UI/Replacements/GovernmentScreen_MiniCivVI.lua
+++ src/UI/Replacements/GovernmentScreen_MiniCivVI.lua
@@ -3,7 +3,16 @@ include "GovernmentScreen";
 -- Copy the original function from the game
 local BASE_RealizeTabs = RealizeTabs;
 
+-- Override the RealizeTabs function to hide the policies tab button
 function RealizeTabs()
-	BASE_RealizeTabs();
+    BASE_RealizeTabs();
+    -- Hide the policies tab button
     Controls.ButtonPolicies:SetHide(true);
+    -- Remove the gap caused by hiding the button
+    Controls.ButtonPolicies:SetSizeX(0);
 end
+
+-- If the policies tab gets opened (for example, this will happen automatically after
+-- accepting a government change), switch to the governments tab.
+---@diagnostic disable-next-line: undefined-global
+LuaEvents.GovernmentScreen_PolicyTabOpen.Add(OnOpenGovernmentScreenGovernments);
--- src/UI/Replacements/TechCivicCompletedPopup_MiniCivVI.lua
+++ src/UI/Replacements/TechCivicCompletedPopup_MiniCivVI.lua
@@ -6,27 +6,28 @@ local BASE_ShowCivicCompletedPopup = ShowCivicCompletedPopup;
 function ShowCivicCompletedPopup( player, civic, quote, audio )
     BASE_ShowCivicCompletedPopup(player, civic, quote, audio);
 
-	local civicInfo = GameInfo.Civics[civic];
-	if civicInfo == nil then
-		UI.DataError("Cannot show civic popup because GameInfo.Civics["..tostring(civic).."] doesn't have data.");
-		return;
-	end
+    local civicInfo = GameInfo.Civics[civic];
+    if civicInfo == nil then
+        UI.DataError("Cannot show civic popup because GameInfo.Civics["..tostring(civic).."] doesn't have data.");
+        return;
+    end
 
     local civicType = civicInfo.CivicType;
+    ---@diagnostic disable-next-line: undefined-global
     local unlockableTypes = GetUnlockablesForCivic(civicType, player);
     local isCivicUnlockGovernmentType = false;
 
-	-- Determine if we've unlocked a new government type
-	for _,unlockItem in ipairs(unlockableTypes) do
-		local typeInfo = GameInfo.Types[unlockItem[1]];
-		if(typeInfo and typeInfo.Kind == "KIND_GOVERNMENT") then
-			isCivicUnlockGovernmentType = true;
-		end
-	end
+    -- Determine if we've unlocked a new government type
+    for _,unlockItem in ipairs(unlockableTypes) do
+        local typeInfo = GameInfo.Types[unlockItem[1]];
+        if(typeInfo and typeInfo.Kind == "KIND_GOVERNMENT") then
+            isCivicUnlockGovernmentType = true;
+        end
+    end
 
     -- In the original game, the government button becomes "Show Policies" if a new
     -- government isn't unlocked. Instead, hide it altogether.
-	if not isCivicUnlockGovernmentType then
-		Controls.ChangeGovernmentButton:SetHide(true);
-	end
+    if not isCivicUnlockGovernmentType then
+        Controls.ChangeGovernmentButton:SetHide(true);
+    end
 end
