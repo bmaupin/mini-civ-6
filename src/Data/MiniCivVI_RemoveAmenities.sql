32c24df6942fe11db27d45da141cfe5659dc47ba
bmaupin <bmaupin@users.noreply.github.com>
2025-07-22 13:32:10 -0400

    feat: Start working on No Amenities option

---
+++ src/Data/MiniCivVI_RemoveAmenities.sql
@@ -0,0 +1,9 @@
+DELETE FROM Types WHERE Type = 'CAPABILITY_AMENITIES';
+
+-- TODO:
+-- 1. test that city amenity HUD is empty or removed (see screenshot)
+-- 1. does this disable entertainment complex district?
+-- 1. does this disable games and recreation civic?
+
+-- <Row Type="DISTRICT_ENTERTAINMENT_COMPLEX" Kind="KIND_DISTRICT"/>
+-- <Row Type="CIVIC_GAMES_RECREATION" Kind="KIND_CIVIC"/>
