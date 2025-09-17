ba073c0e3c872854462ce87e0ecf15f10d43f843
bmaupin <bmaupin@users.noreply.github.com>
2025-09-16 20:00:33 -0400

    feat: Set minimum map size
    Currently 20x12. Smaller sizes caused crashes; initially the duel map size was 16x10.

--- src/Data/MiniCivVI_SmallerMaps.sql
+++ src/Data/MiniCivVI_SmallerMaps.sql
@@ -2,13 +2,14 @@ UPDATE Maps
 SET
     GridWidth = CASE
         -- Calculate 40%, floor by casting to INT, then subtract 1 if odd
+        -- Use MAX to set minimum map size; smaller sizes caused crashes
         WHEN CAST(GridWidth * 0.4 AS INTEGER) % 2 = 1
-            THEN CAST(GridWidth * 0.4 AS INTEGER) - 1
-        ELSE CAST(GridWidth * 0.4 AS INTEGER)
+            THEN MAX(20, CAST(GridWidth * 0.4 AS INTEGER) - 1)
+        ELSE MAX(20, CAST(GridWidth * 0.4 AS INTEGER))
     END,
     GridHeight = CASE
         WHEN CAST(GridHeight * 0.4 AS INTEGER) % 2 = 1
-            THEN CAST(GridHeight * 0.4 AS INTEGER) - 1
-        ELSE CAST(GridHeight * 0.4 AS INTEGER)
+            THEN MAX(12, CAST(GridHeight * 0.4 AS INTEGER) - 1)
+        ELSE MAX(12, CAST(GridHeight * 0.4 AS INTEGER))
     END
 WHERE MapSizeType IN ('MAPSIZE_DUEL', 'MAPSIZE_TINY', 'MAPSIZE_SMALL', 'MAPSIZE_STANDARD', 'MAPSIZE_LARGE', 'MAPSIZE_HUGE');
