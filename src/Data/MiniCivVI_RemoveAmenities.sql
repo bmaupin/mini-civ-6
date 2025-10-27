9ab30245de67654d76839e95ab4ccec3bc5ed59e
bmaupin <bmaupin@users.noreply.github.com>
2025-10-27 12:37:30 -0400

    feat: Comment out No Amenities option for now
    It's not finished; needs more testing, UI changes, text, and probably the entertainment district needs to be removed as well.

--- src/Data/MiniCivVI_RemoveAmenities.sql
+++ src/Data/MiniCivVI_RemoveAmenities.sql
@@ -1,9 +1,13 @@
 DELETE FROM Types WHERE Type = 'CAPABILITY_AMENITIES';
+DELETE FROM Types WHERE Type = 'CIVIC_GAMES_RECREATION';
 
 -- TODO:
--- 1. test that city amenity HUD is empty or removed (see screenshot)
--- 1. does this disable entertainment complex district?
--- 1. does this disable games and recreation civic?
+-- 1. [x] test that city amenity HUD is empty or removed (see screenshot)
+--    - Still showing up in UI: amenities from luxury resources, entertainment
+-- 1. [x] does this disable entertainment complex district? no
+-- 1. [x] does this disable games and recreation civic? no
+-- 1. [ ] Hide amenities panel altogether? Should be easy
+--    👉 Do this later; for now leave it as-is to see if amenities have an impact
+--    - Override OnSelectHealthTab, call it then call Controls.PanelAmenities:SetHide(true) ??
 
 -- <Row Type="DISTRICT_ENTERTAINMENT_COMPLEX" Kind="KIND_DISTRICT"/>
--- <Row Type="CIVIC_GAMES_RECREATION" Kind="KIND_CIVIC"/>
