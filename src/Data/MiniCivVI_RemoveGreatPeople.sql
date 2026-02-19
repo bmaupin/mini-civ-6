7e5d98e8a825bac649bc04e2a82f002c4e069647
bmaupin <bmaupin@users.noreply.github.com>
2026-02-18 20:28:59 -0500

    fix: Remove great person points
    Without this, we still accrue points and can get great people

--- src/Data/MiniCivVI_RemoveGreatPeople.sql
+++ src/Data/MiniCivVI_RemoveGreatPeople.sql
@@ -2,5 +2,11 @@ DELETE FROM Types
 WHERE Type = 'BUILDING_ORACLE'
   OR Type = 'CAPABILITY_GREAT_PEOPLE';
 
+DELETE FROM Building_GreatPersonPoints;
+DELETE FROM Building_GreatWorks;
+DELETE FROM District_CitizenGreatPersonPoints;
+DELETE FROM District_GreatPersonPoints;
+DELETE FROM Project_GreatPersonPoints;
+
 INSERT INTO CivilopediaSectionExcludes
 VALUES ('GREATPEOPLE');
