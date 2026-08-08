33f8f302866690e9957bfa393aaa14a65d434a13
bmaupin <bmaupin@users.noreply.github.com>
2026-08-08 15:57:55 -0400

    feat: Automate lumber mills

--- src/Scripts/AutomateImprovements.lua
+++ src/Scripts/AutomateImprovements.lua
@@ -1,7 +1,10 @@
 local NO_IMPROVEMENT = -1;
 local NO_TEAM = -1;
 
+local FEATURE_FOREST_INDEX = GameInfo.Features["FEATURE_FOREST"].Index;
+
 local IMPROVEMENT_FARM_INDEX = GameInfo.Improvements["IMPROVEMENT_FARM"].Index;
+local IMPROVEMENT_LUMBER_MILL_INDEX = GameInfo.Improvements["IMPROVEMENT_LUMBER_MILL"].Index;
 
 local TERRAIN_GRASS_INDEX = GameInfo.Terrains["TERRAIN_GRASS"].Index;
 local TERRAIN_PLAINS_INDEX = GameInfo.Terrains["TERRAIN_PLAINS"].Index;
@@ -25,9 +28,13 @@ function OnCityTileOwnershipChanged(ownerID, _cityID, plotX, plotY)
     if plot ~= nil and not PlotHasImprovement(plot) then
         local improvementIndex = nil;
         local improvementType = nil;
+
+        local featureType = plot:GetFeatureType();
         local terrainType = plot:GetTerrainType();
 
+        print("**************************************** featureType=" .. tostring(featureType));
         print("**************************************** terrainType=" .. tostring(terrainType));
+        print("**************************************** FEATURE_FOREST_INDEX=" .. tostring(FEATURE_FOREST_INDEX));
         print("**************************************** TERRAIN_GRASS_INDEX=" .. tostring(TERRAIN_GRASS_INDEX));
         print("**************************************** TERRAIN_PLAINS_INDEX=" .. tostring(TERRAIN_PLAINS_INDEX));
 
@@ -35,6 +42,11 @@ function OnCityTileOwnershipChanged(ownerID, _cityID, plotX, plotY)
           terrainType == TERRAIN_PLAINS_INDEX) then
             improvementIndex = IMPROVEMENT_FARM_INDEX;
             improvementType = "IMPROVEMENT_FARM";
+        end
+
+        if (featureType == FEATURE_FOREST_INDEX) then
+            improvementIndex = IMPROVEMENT_LUMBER_MILL_INDEX;
+            improvementType = "IMPROVEMENT_LUMBER_MILL";
         end
 
         print("**************************************** improvementIndex=" .. tostring(improvementIndex));
