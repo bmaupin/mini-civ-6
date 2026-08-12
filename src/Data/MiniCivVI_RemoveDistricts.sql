c220a9a1f2b2751edcd9aa76381dd19517cf62ef
bmaupin <bmaupin@users.noreply.github.com>
2026-08-12 13:12:36 -0400

    feat: Re-enable some districts
    Re-enable a limited number of districts which seem more essential

--- src/Data/MiniCivVI_RemoveDistricts.sql
+++ src/Data/MiniCivVI_RemoveDistricts.sql
@@ -1,3 +1,24 @@
+-- Keep these districts
+CREATE TEMP TABLE IF NOT EXISTS DistrictsToKeep AS
+  SELECT DistrictType FROM Districts WHERE DistrictType IN (
+    -- GS only, unique placement and benefits
+    'DISTRICT_CANAL',
+    'DISTRICT_CITY_CENTER',
+    -- Unique harbour replacement
+    'DISTRICT_COTHON',
+    -- GS only, unique placement and benefits
+    'DISTRICT_DAM',
+    -- Allows inland cities access to build boats, only built on water
+    'DISTRICT_HARBOR',
+    -- Unique harbour replacement
+    'DISTRICT_ROYAL_NAVY_DOCKYARD',
+    -- Required for science victory, shows victory progress on the map
+    'DISTRICT_SPACEPORT',
+    -- The only scenario-specific district (Black Death)
+    'DISTRICT_WALLED_QUARTER',
+    'DISTRICT_WONDER'
+  );
+
 UPDATE Buildings
 SET PrereqTech = (
   SELECT PrereqTech FROM Districts
@@ -7,7 +28,7 @@ SET PrereqTech = (
 WHERE PrereqCivic IS NULL
   AND PrereqTech IS NULL
   AND PrereqDistrict IS NOT NULL
-  AND PrereqDistrict != 'DISTRICT_CITY_CENTER';
+  AND PrereqDistrict NOT IN (SELECT DistrictType FROM DistrictsToKeep);
 
 UPDATE Buildings
 SET PrereqCivic = (
@@ -18,11 +39,13 @@ SET PrereqCivic = (
 WHERE PrereqCivic IS NULL
   AND PrereqTech IS NULL
   AND PrereqDistrict IS NOT NULL
-  AND PrereqDistrict != 'DISTRICT_CITY_CENTER';
+  AND PrereqDistrict NOT IN (SELECT DistrictType FROM DistrictsToKeep);
 
+-- Buildings without a PrereqDistrict can't be built
 UPDATE Buildings
 SET PrereqDistrict = 'DISTRICT_CITY_CENTER'
-WHERE PrereqDistrict IS NOT NULL;
+WHERE PrereqDistrict IS NOT NULL
+  AND PrereqDistrict NOT IN (SELECT DistrictType FROM DistrictsToKeep);
 
 UPDATE Projects
 SET PrereqTech = (
@@ -33,7 +56,7 @@ SET PrereqTech = (
 WHERE PrereqCivic IS NULL
   AND PrereqTech IS NULL
   AND PrereqDistrict IS NOT NULL
-  AND PrereqDistrict != 'DISTRICT_CITY_CENTER';
+  AND PrereqDistrict NOT IN (SELECT DistrictType FROM DistrictsToKeep);
 
 UPDATE Projects
 SET PrereqCivic = (
@@ -44,11 +67,12 @@ SET PrereqCivic = (
 WHERE PrereqCivic IS NULL
   AND PrereqTech IS NULL
   AND PrereqDistrict IS NOT NULL
-  AND PrereqDistrict != 'DISTRICT_CITY_CENTER';
+  AND PrereqDistrict NOT IN (SELECT DistrictType FROM DistrictsToKeep);
 
 UPDATE Projects
 SET PrereqDistrict = 'DISTRICT_CITY_CENTER'
-WHERE PrereqDistrict IS NOT NULL;
+WHERE PrereqDistrict IS NOT NULL
+  AND PrereqDistrict NOT IN (SELECT DistrictType FROM DistrictsToKeep);
 
 UPDATE Units
 SET PrereqTech = (
@@ -59,7 +83,7 @@ SET PrereqTech = (
 WHERE PrereqCivic IS NULL
   AND PrereqTech IS NULL
   AND PrereqDistrict IS NOT NULL
-  AND PrereqDistrict != 'DISTRICT_CITY_CENTER';
+  AND PrereqDistrict NOT IN (SELECT DistrictType FROM DistrictsToKeep);
 
 UPDATE Units
 SET PrereqCivic = (
@@ -70,37 +94,45 @@ SET PrereqCivic = (
 WHERE PrereqCivic IS NULL
   AND PrereqTech IS NULL
   AND PrereqDistrict IS NOT NULL
-  AND PrereqDistrict != 'DISTRICT_CITY_CENTER';
+  AND PrereqDistrict NOT IN (SELECT DistrictType FROM DistrictsToKeep);
 
 UPDATE Units
 SET PrereqDistrict = 'DISTRICT_CITY_CENTER'
-WHERE PrereqDistrict IS NOT NULL;
+WHERE PrereqDistrict IS NOT NULL
+  AND PrereqDistrict NOT IN (SELECT DistrictType FROM DistrictsToKeep);
 
 UPDATE GreatPersonClasses
 SET DistrictType = 'DISTRICT_CITY_CENTER'
-WHERE DistrictType IS NOT NULL;
+WHERE DistrictType IS NOT NULL
+  AND DistrictType NOT IN (SELECT DistrictType FROM DistrictsToKeep);
 
 UPDATE GreatPersonIndividuals
 SET ActionRequiresCompletedDistrictType = 'DISTRICT_CITY_CENTER'
-WHERE ActionRequiresCompletedDistrictType IS NOT NULL;
+WHERE ActionRequiresCompletedDistrictType IS NOT NULL
+  AND ActionRequiresCompletedDistrictType NOT IN (SELECT DistrictType FROM DistrictsToKeep);
 
 UPDATE MajorStartingUnits
 SET District = 'DISTRICT_CITY_CENTER'
-WHERE District IS NOT NULL;
+WHERE District IS NOT NULL
+  AND District NOT IN (SELECT DistrictType FROM DistrictsToKeep);
 
 UPDATE StartingBuildings
 SET District = 'DISTRICT_CITY_CENTER'
-WHERE District IS NOT NULL;
+WHERE District IS NOT NULL
+  AND District NOT IN (SELECT DistrictType FROM DistrictsToKeep);
 
 UPDATE UnitOperations
 SET TargetDistrict = 'DISTRICT_CITY_CENTER'
-WHERE TargetDistrict IS NOT NULL;
+WHERE TargetDistrict IS NOT NULL
+  AND TargetDistrict NOT IN (SELECT DistrictType FROM DistrictsToKeep);
 
 -- Deleting districts can cause crashes because some districts are referenced in Lua
 -- files, so disable them instead
 UPDATE Districts
   SET MaxPerPlayer = 0,
-    -- This makes it so the districts don't show up in the civics/tech tree
+    -- This makes it so the disabled districts don't show up in the civics/tech tree
     PrereqCivic = NULL,
     PrereqTech = NULL
-  WHERE DistrictType != 'DISTRICT_CITY_CENTER';
+  WHERE DistrictType NOT IN (SELECT DistrictType FROM DistrictsToKeep);
+
+DROP TABLE IF EXISTS DistrictsToKeep;
