70e3f20c16104afcedc6e71ddff858f5dfe6ca20
bmaupin <bmaupin@users.noreply.github.com>
2026-08-13 13:25:21 -0400

    feat: Remove encampent and industrial zone projects
    ... if No Great People is enabled

--- src/Data/MiniCivVI_RemoveGreatPeople.sql
+++ src/Data/MiniCivVI_RemoveGreatPeople.sql
@@ -1,6 +1,13 @@
 DELETE FROM Types
-WHERE Type = 'BUILDING_ORACLE'
-  OR Type = 'CAPABILITY_GREAT_PEOPLE';
+WHERE Type IN (
+  'BUILDING_ORACLE',
+  'CAPABILITY_GREAT_PEOPLE',
+  -- This only provides gold and great general points, but commercial hub project is a
+  -- better fit for just gold
+  'PROJECT_ENHANCE_DISTRICT_ENCAMPMENT',
+  -- Similarly, this only provides gold and great engineer points
+  'PROJECT_ENHANCE_DISTRICT_INDUSTRIAL_ZONE'
+);
 
 DELETE FROM Building_GreatPersonPoints;
 DELETE FROM Building_GreatWorks;
