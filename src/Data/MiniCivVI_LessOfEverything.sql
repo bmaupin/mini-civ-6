d0ddef7c6a7b5a597e55b56ba9d11aab8b075b84
bmaupin <bmaupin@users.noreply.github.com>
2026-08-17 13:51:34 -0400

    feat: Start working on trimming down other systems

---
+++ src/Data/MiniCivVI_LessOfEverything.sql
@@ -0,0 +1,40 @@
+-- Keep these districts
+CREATE TEMP TABLE IF NOT EXISTS DistrictsToDisable AS
+  SELECT DistrictType FROM Districts WHERE DistrictType IN (
+    -- Theatre Square replacement
+    'DISTRICT_ACROPOLIS',
+    -- Housing
+    'DISTRICT_AQUEDUCT',
+    -- Aqueduct replacement
+    'DISTRICT_BATH',
+    -- Culture, city states, espionage
+    'DISTRICT_DIPLOMATIC_QUARTER',
+    -- Neighbourhood replacement
+    'DISTRICT_MBANZA',
+    -- Housing
+    'DISTRICT_NEIGHBORHOOD',
+    -- Housing, appeal, culture bomb
+    'DISTRICT_PRESERVE',
+    -- Culture, great person points
+    'DISTRICT_THEATER'
+  );
+
+DELETE FROM Buildings
+WHERE PrereqDistrict IN (SELECT DistrictType FROM DistrictsToDisable);
+
+DELETE FROM Projects
+WHERE PrereqDistrict IN (SELECT DistrictType FROM DistrictsToDisable);
+
+DELETE FROM Units
+WHERE PrereqDistrict IN (SELECT DistrictType FROM DistrictsToDisable);
+
+-- Deleting districts can cause crashes because some districts are referenced in Lua
+-- files, so disable them instead
+UPDATE Districts
+SET MaxPerPlayer = 0,
+  -- This makes it so the districts don't show up in the civics/tech tree
+  PrereqCivic = NULL,
+  PrereqTech = NULL
+WHERE DistrictType IN (SELECT DistrictType FROM DistrictsToDisable);
+
+DROP TABLE IF EXISTS DistrictsToDisable;
